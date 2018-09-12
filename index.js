847db7faa5fe156d77c0f7e4a73b38bb71bd3c1b
Oybekcha <40931210+Oybekcha@users.noreply.github.com>
2018-09-12 18:06:31 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-const TelegramBot = require('node-telegram-bot-api')
+const Bot = require('node-telegram-bot-api');
+const request = require('request');
+const token = '557457250:AAEGwpwQeVgMaPYH_gsuhoOkE7kV8nUtvIs';
+const url = 'https://launchlibrary.net/1.3/launch';
+const trigger = 'I want to travel!';
+const bot = new Bot(token, {polling: true});
 
-const TOKEN ='557457250:AAEGwpwQeVgMaPYH_gsuhoOkE7kV8nUtvIs'
-
-const bot = new TelegramBot(TOKEN, {polling: true})
-
-bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, `Salom quyon botim, shu safar ishlarsanda " Hi ${msg.from.first_name}"`)
-})
+const prepareData = (body) => {
+    const launches = JSON.parse(body).launches;
+    return launches.filter((launch) => launch !== undefined)
+        .map((launch) => `${launch.name} on ${launch.net}`)
+        .join('\n\n');
+};
+bot.on('message', (msg) => {
+    if (msg.text.toString() === trigger) {
+        return request(url, (err, resp, body) => {
+            bot.sendMessage(msg.chat.id, prepareData(body));
+        });
+    }
+    bot.sendMessage(msg.chat.id, 'Hi, do you want to travel?', {
+            reply_markup: {
+                keyboard: [[trigger], ['Bulk option']]
+            }
+        }
+    );
+});
